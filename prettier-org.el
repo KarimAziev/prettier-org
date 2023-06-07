a315b1912db37585a5799f8b9cc9bb2237fa0806
KarimAziev <karim.aziiev@gmail.com>
2023-06-07 16:47:10 +0300

    add mode for edit src buffers

--- prettier-org.el
+++ prettier-org.el
@@ -87,8 +87,9 @@
 
 (defcustom prettier-org-langs-formatters nil
   "Alist of org src languages and corresponding custom formatters."
-  :type '(alist :key-type (string :tag "Language")
-                :value-type (function :tag "Custom function"))
+  :type '(alist
+					:key-type (string :tag "Language")
+          :value-type (function :tag "Custom function"))
   :group 'prettier-org)
 
 (defcustom prettier-org-args '()
@@ -196,10 +197,11 @@ of replacements and original values"
           (cons code '()))))))
 
 (defun prettier-org-commment-noweb-0 ()
-  "Wrap noweb referneces in CODE in comments according to LANG.
+	"Wrap noweb referneces in CODE in comments according to LANG.
 Return alist of replacements and original values."
-  (let ((replacements)
-        (re (org-babel-noweb-wrap)))
+	(let ((replacements)
+        (re (org-babel-noweb-wrap))
+				(inhibit-read-only t))
     (while (re-search-forward re nil t 1)
       (let ((beg (match-beginning 0))
             (end (match-end 0))
@@ -223,32 +225,39 @@ replace with."
     (buffer-string)))
 
 (defun prettier-org-format-region (lang beg end &rest options)
-  "Format region between BEG and END with LANG and OPTIONS."
-  (setq options (flatten-list options))
+	"Format region between BEG and END with LANG and OPTIONS."
+	(setq options (flatten-list options))
   (let ((code (buffer-substring-no-properties beg end))
         (parser (or (car (seq-drop (member "--parser" options) 1)))))
-    (let ((prefix (when (and (string-prefix-p "{" (string-trim code))
-                             (null (member parser '("json" "json5"))))
-                    "let temp_var = "))
+    (let ((prefix
+					 (when (and (string-prefix-p "{" (string-trim code))
+                      (null (member parser '("json" "json5"))))
+             "let temp_var = "))
           (cell))
       (setq cell (prettier-org-commment-noweb
                   lang
                   (if prefix
                       (concat prefix code)
                     code)))
-      (let ((result
-             (prettier-org-format-string
-              (car cell)
-              options)))
+      (let* ((result
+							(prettier-org-format-string
+               (car cell)
+               options))
+						 (str
+							(when (car result)
+								(if prefix
+                    (substring
+                     (cadr result)
+                     (length prefix))
+                  (cadr result))))
+						 (final
+							(when str
+								(prettier-org-replace-matches
+                 (cdr cell)
+                 str))))
         (if (car result)
             (replace-region-contents beg end (lambda ()
-                                               (prettier-org-replace-matches
-                                                (cdr cell)
-                                                (if prefix
-                                                    (substring
-                                                     (cadr result)
-                                                     (length prefix))
-                                                  (cadr result)))))
+                                               final))
           (message (cadr result)))))))
 
 (defun prettier-org-custom-formatter (lang beg end)
@@ -269,14 +278,97 @@ replace with."
       (replace-region-contents beg end
                                (lambda () formatted)))))
 
+(defun prettier-org-find-lang-by-mode ()
+	"Find current major-mode in `org-src-lang-modes'."
+	(let ((mode (symbol-name major-mode)))
+		(seq-find (lambda (it)
+								(string= mode
+												 (format "%s-mode" (cdr it))))
+							org-src-lang-modes)))
+
+(defun prettier-org-next-read-only-property-change ()
+  "Jump to the position of next read-only property change.
+Return the position of point if found, or nil."
+  (when-let ((beg (next-single-property-change (point) 'read-only)))
+    (goto-char beg)
+    beg))
+
+(defun prettier-org-buffer-format-region (beg end)
+	"Run PRETTIER-FN with ARGS on region between BEG and END."
+	(let* ((buff (current-buffer))
+         (content (buffer-substring-no-properties
+                   beg
+                   end))
+				 (mode major-mode)
+         (formatted (with-temp-buffer
+											(insert content)
+											(goto-char (point-min))
+											(let ((reps (prettier-org-commment-noweb-0)))
+												(funcall mode)
+												(goto-char (point-min))
+												(save-excursion
+													(re-search-forward "[.]" nil t 1)
+													(run-hooks 'before-save-hook))
+												(prettier-org-replace-matches
+												 reps
+												 (buffer-substring-no-properties (point-min)
+																												 (point-max)))))))
+    (when (and formatted
+               (not (string= content formatted)))
+      (with-current-buffer buff
+				(let ((inhibit-read-only t))
+					(replace-region-contents beg end (lambda () formatted))
+					formatted)))))
+
+(defun prettier-org-format-non-readonly-regions ()
+	"Run `before-save-hook' in temporarly buffer with commented noweb references.
+Also skip read-only properties.
+Supposed to use in `org-src-mode-hook'."
+	(cond ((save-excursion
+					 (goto-char (point-min))
+					 (prettier-org-next-read-only-property-change))
+				 (save-excursion
+					 (let* ((fn (lambda ()
+												(let* ((start (point))
+															 (end
+																(or
+																 (prettier-org-next-read-only-property-change)
+																 (point-max))))
+													(unless (or (bobp)
+																			(eobp))
+														(when (and (get-text-property (1- (point))
+																													'read-only)
+																			 (not (get-text-property (1+ (point))
+																															 'read-only)))
+															(forward-char 1)
+															(setq end (point))))
+													(prettier-org-buffer-format-region start end)))))
+						 (goto-char (point-min))
+						 (unless (get-text-property (point) 'read-only)
+							 (funcall fn))
+						 (while
+								 (when (get-text-property (point) 'read-only)
+									 (prettier-org-next-read-only-property-change))
+							 (funcall fn)))))
+				(t
+				 (let ((reps
+								(save-excursion
+									(goto-char (point-min))
+									(prettier-org-commment-noweb-0))))
+					 (run-hooks 'before-save-hook)
+					 (prettier-org-replace-matches
+						reps
+						(buffer-substring-no-properties (point-min)
+																						(point-max)))))))
+
 (defun prettier-org-format (&optional argument)
-  "Format src body at point with prettier if corresponding parser found.
+	"Format src body at point with prettier if corresponding parser found.
 Parsers for src languages listed in `prettier-org-src-parsers-alist'.
 Common options listed in `prettier-org-args'.
 Alternatively if ARGUMENT is non-nil (interactively, with prefix argument),
 read prettier options in minibuffer."
-  (interactive "P")
-  (when-let* ((params (prettier-org-get-prettier-params))
+	(interactive "P")
+	(when-let* ((params (prettier-org-get-prettier-params))
               (lang (car params)))
     (if-let* ((custom-formatter
                (cdr (assoc lang prettier-org-langs-formatters)))
@@ -293,7 +385,8 @@ read prettier options in minibuffer."
                            (cdr cell)
                            formatted))
           (unless (string= code formatted)
-            (replace-region-contents (nth 1 params) (nth 2 params)
+            (replace-region-contents (nth 1 params)
+																		 (nth 2 params)
                                      (lambda ()
                                        formatted))))
       (let* ((parser (cdr (assoc lang prettier-org-src-parsers-alist)))
@@ -318,7 +411,8 @@ read prettier options in minibuffer."
                   (append (list "--parser" parser)
                           prettier-org-args)))))
         (when options
-          (prettier-org-format-region lang (nth 1 params) (nth 2 params)
+          (prettier-org-format-region lang (nth 1 params)
+																			(nth 2 params)
                                       options))))))
 
 (defun prettier-org-format-all-src-blocks (&optional file)
@@ -332,13 +426,32 @@ read prettier options in minibuffer."
           (prettier-org-format-region lang beg-body end-body "--parser" parser
                                       prettier-org-args))))))
 
+(defun prettier-org-src-run-save-hooks ()
+	"Run wrapped before save hooks after `org-edit-src-save'."
+	(when (eq this-command 'org-edit-src-save)
+		(prettier-org-format-non-readonly-regions)))
+
+;;;###autoload
+(define-minor-mode prettier-org-edit-src-mode
+  "Run `before-save-hook' on `org-edit-src-save' with commented noweb references.
+Also skip read-only properties.
+
+Supposed to use in `org-src-mode-hook'.
+
+Usage:
+\=(add-hook \='org-src-mode-hook #\='prettier-org-edit-src-mode)."
+  :global nil
+  (if prettier-org-edit-src-mode
+      (add-hook 'pre-command-hook #'prettier-org-src-run-save-hooks nil t)
+    (remove-hook 'pre-command-hook #'prettier-org-src-run-save-hooks t)))
+
 ;;;###autoload
 (define-minor-mode prettier-org-mode
   "Runs prettier on save when point is inside org src block."
   :global nil
   (if prettier-org-mode
-      (add-hook 'before-save-hook 'prettier-org-format nil 'local)
-    (remove-hook 'before-save-hook 'prettier-org-format 'local)))
+      (add-hook 'before-save-hook #'prettier-org-format nil 'local)
+    (remove-hook 'before-save-hook #'prettier-org-format 'local)))
 
 (provide 'prettier-org)
 ;;; prettier-org.el ends here
