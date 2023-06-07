6fdd5978670c6cf08c3a2443e6e4a48d5fcf60af
KarimAziev <karim.aziiev@gmail.com>
2023-06-07 16:47:43 +0300

    v0.2.0

--- prettier-org.el
+++ prettier-org.el
@@ -5,7 +5,7 @@
 ;; Author: Karim Aziiev <karim.aziiev@gmail.com>
 ;; URL: https://github.com/KarimAziev/prettier-org
 ;; Keywords: convenience, languages
-;; Version: 0.1.1
+;; Version: 0.2.0
 ;; Package-Requires: ((emacs "27.1"))
 
 ;; This file is NOT part of GNU Emacs.
